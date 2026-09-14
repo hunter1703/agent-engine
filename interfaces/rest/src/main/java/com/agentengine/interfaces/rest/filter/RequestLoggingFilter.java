9c5286578666df8d84ce9d6b7d96d57158b39e75
rahul <rahulhpatel1998@gmail.com>
2026-09-14 11:24:25 +0530

    add header for request processing

--- interfaces/rest/src/main/java/com/agentengine/interfaces/rest/filter/RequestLoggingFilter.java
+++ interfaces/rest/src/main/java/com/agentengine/interfaces/rest/filter/RequestLoggingFilter.java
@@ -14,9 +14,12 @@ public class RequestLoggingFilter implements ContainerRequestFilter, ContainerRe
 
   private static final Logger LOG = LoggerFactory.getLogger(RequestLoggingFilter.class);
   private static final String TRACE_ID_HEADER = "X-Trace-ID";
+  private static final String START_TIME_KEY = "start-time";
+  private static final String PROCESSING_TIME_HEADER = "X-Backend-Processing-Time-Ms";
 
   @Override
   public void filter(final ContainerRequestContext requestContext) {
+    requestContext.setProperty(START_TIME_KEY, System.nanoTime());
     LOG.debug(
         "Request received method={} path={}",
         requestContext.getMethod(),
@@ -27,10 +30,22 @@ public class RequestLoggingFilter implements ContainerRequestFilter, ContainerRe
   public void filter(
       final ContainerRequestContext requestContext,
       final ContainerResponseContext responseContext) {
+
+    final Long startTime = (Long) requestContext.getProperty(START_TIME_KEY);
+    final long duration = startTime != null ? System.nanoTime() - startTime : -1;
+
+    if (duration >= 0) {
+      responseContext
+          .getHeaders()
+          .add(PROCESSING_TIME_HEADER, String.valueOf(duration / 1_000_000));
+    }
+
     final String traceId = Span.current().getSpanContext().getTraceId();
     if (Span.current().getSpanContext().isValid()) {
       responseContext.getHeaders().add(TRACE_ID_HEADER, traceId);
     }
-    LOG.debug("Request completed status={}", responseContext.getStatus());
+
+    final long durationMs = duration >= 0 ? (duration / 1_000_000) : -1;
+    LOG.debug("Request completed status={} durationMs={}", responseContext.getStatus(), durationMs);
   }
 }
